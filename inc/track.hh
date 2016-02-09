5d106e87a9e2ea4c821d23dc95a954f9a1276ebc
wno-git <wno-git@users.noreply.github.com>
2016-02-10 00:21:47 +0200

    Remove unneeded things from Track

--- inc/track.hh
+++ inc/track.hh
@@ -1,18 +1,10 @@
 #pragma once
 
-#include <string>
-#include <cstdint>
-
 namespace syn {
 
 class Track {
 public:
     Track();
-
-    bool hasTrigger(const uint32_t time);
-
-private:
-    std::string notes;
 
 };
 
--- src/track.cc
+++ src/track.cc
@@ -1,9 +1,4 @@
 #include "track.hh"
 
-syn::Track::Track():
-    notes() {
-}
-
-bool syn::Track::hasTrigger(const uint32_t time) {
-
+syn::Track::Track() {
 }

75d3844c8f16e98ca64a7cf26e360ab604312963
wno-git <wno-git@users.noreply.github.com>
2016-02-10 00:46:51 +0200

    Load empty track

--- test/unit_track.cc
+++ test/unit_track.cc
@@ -1,4 +1,11 @@
 #include <gtest/gtest.h>
+#include <sstream>
 
-TEST(unit_track, load_track) {
+#include "track.hh"
+
+TEST(unit_track, load_empty_track) {
+    std::istringstream json("{}");
+
+    syn::Track track;
+    EXPECT_TRUE(track.loadFile(json));
 }

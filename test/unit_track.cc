500993b5db5079fb6c22bd78557802954d909d43
wno-git <wno-git@users.noreply.github.com>
2016-02-17 00:24:28 +0200

    Create separate testcase for Track integration tests

--- test/unit_track.cc
+++ test/unit_track.cc
@@ -17,29 +17,3 @@ TEST(unit_track, load_bad_json) {
     syn::Track track;
     ASSERT_THROW(track.loadFile(json), std::invalid_argument);
 }
-
-TEST(unit_track, load_generators) {
-    std::istringstream json(R"(
-        {
-            "generators": [
-                {
-                    "name": "g1"
-                },
-                {
-                    "name": "g2"
-                }
-            ]
-        }
-    )");
-
-    syn::Track track;
-    track.loadFile(json);
-
-    ASSERT_EQ(track.numGenerators(), 2);
-
-    ASSERT_NO_THROW(track.getGenerator("g1"));
-    ASSERT_NO_THROW(track.getGenerator("g2"));
-
-    ASSERT_EQ(track.getGenerator("g1").getName(), "g1");
-    ASSERT_EQ(track.getGenerator("g2").getName(), "g2");
-}

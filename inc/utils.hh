4538da53d91725dea7d3ca912b7949ddb3b0790b
wno-git <wno-git@users.noreply.github.com>
2016-02-18 23:40:33 +0200

    Add missing digit

--- inc/utils.hh
+++ inc/utils.hh
@@ -10,7 +10,7 @@ class Utils {
 public:
     Utils() = delete;
 
-    static constexpr double pi2 = 3.1415 * 2.0;
+    static constexpr double pi2 = 3.14159 * 2.0;
 
     static std::int16_t dToS16(double i) {
         // TODO: investigate if there's a better way to do this

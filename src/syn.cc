22268fad09cd49d8d64e8ca7ee1c2a7ecbfb30b7
wno-git <wno-git@users.noreply.github.com>
2016-02-18 23:34:28 +0200

    Write buffers from track to output

--- src/syn.cc
+++ src/syn.cc
@@ -1,14 +1,13 @@
 #include <iostream>
 #include <fstream>
 #include <cstdint>
+#include <cstddef>
+#include <array>
+#include <algorithm>
 #include "utils.hh"
-#include "sequencer.hh"
 #include "track.hh"
 
 int main(int argc, char** argv) {
-    const int samplerate = 44100;
-    syn::Sequencer sequencer(samplerate);
-
     if (argc != 2) {
         std::cerr << "Invalid arguments" << std::endl;
         return 1;
@@ -26,11 +25,19 @@ int main(int argc, char** argv) {
         return 1;
     }
 
-    while (!sequencer.trackEnd()) {
-        const auto sample = sequencer.nextSample();
-        const auto out = syn::Utils::dToS16(sample);
+    const long samplerate = 44100; // hardcoded for testing
+    std::size_t clock = 0;
+
+    while (true) {
+        const long buflen = 441; // hardcoded for testing
+        const auto buf_d = track.getBuffer(clock, samplerate, buflen);
+        std::array<std::int16_t, buflen> buf_i;
+
+        std::transform(buf_d.begin(), buf_d.end(), buf_i.begin(),
+            syn::Utils::dToS16);
+
         std::cout.write(
-                reinterpret_cast<const char*> (&out),
-                sizeof(std::int16_t));
+                reinterpret_cast<const char*> (buf_i.data()),
+                sizeof(std::int16_t) * buf_i.size());
     }
 }
